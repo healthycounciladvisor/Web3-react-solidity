d64d72d23499cf8686577990df8b0e92d643dd30
healthycounciladvisor <jimm.dev2323@gmail.com>
2021-12-03 13:31:38 -0700

    Updated initial supply value, revert messages

--- hardhat/contracts/TestNetToken.sol
+++ hardhat/contracts/TestNetToken.sol
@@ -9,7 +9,7 @@ pragma solidity ^0.8.4;
 contract TestNetToken {
     bytes32 public name = "TestNetToken";
     bytes32 public symbol = "TNT";
-    uint256 public totalSupply;
+    uint256 public totalSupply = 1000000000000000000000000;
 
     /* token owner => amount of owned tokens */
     mapping(address => uint256) public balanceOf;
@@ -23,9 +23,8 @@ contract TestNetToken {
         uint256 _value
     );
 
-    constructor(uint256 _initialSupply) {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address _to, uint256 _value)
@@ -34,8 +33,9 @@ contract TestNetToken {
     {
         require(
             balanceOf[msg.sender] >= _value,
-            "You don't have enough TNT tokens to complete this transaction."
+            "You don't have enough tokens to complete this transaction."
         );
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
@@ -60,19 +60,19 @@ contract TestNetToken {
     ) public returns (bool success) {
         require(
             _value <= balanceOf[_from],
-            "Original account doesn't have enough TNT tokens to complete this transaction."
+            "Original account doesn't have enough tokens to complete this transaction."
         );
         require(
             _value <= allowance[_from][msg.sender],
-            "You haven't been allocated enough TNT tokens to complete this transaction."
+            "You haven't been allocated enough tokens to complete this transaction."
         );
+
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-
         allowance[_from][msg.sender] -= _value;
 
         emit Transfer(_from, _to, _value);
 
-        return true; // TODO: handle false return value
+        return true;
     }
 }
