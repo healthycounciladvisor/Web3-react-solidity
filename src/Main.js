d4baed2699b92c1571e34c1fef43e04e57774840
healthycounciladvisor <jimm.dev2323@gmail.com>
2021-12-05 14:31:50 -0700

    Started connect app with contract

--- src/Main.js
+++ src/Main.js
@@ -2,25 +2,35 @@ import { useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { ethers } from "ethers";
 
-import { tokenAddress, tokenABI, crowdSaleAddress, crowdSaleABI } from "./utils/contracts";
-
-// TODO: Implement conditional rendering (spinner?)
-// TODO: Connect with contracts
-// TODO: Revise styling
+import { tokenAddress, tokenABI, swapExchangeAddress, swapExchangeABI } from "./utils/contracts";
 
 const Main = () => {
-  const { active, account, library, connector, activate, deactivate } = useWeb3React();
+  const { account, activate, active, chainId, connector, deactivate, error, library, provider, setError } =
+    useWeb3React();
   const [loadingState, setLoadingState] = useState("not-loaded");
   const [tokenPrice, setTokenPrice] = useState();
   const [tokensSold, setTokensSold] = useState();
   const [totalTokens, setTotalTokens] = useState();
   const [userBalance, setUserBalance] = useState();
 
+  useEffect(() => {
+    if (!!library && account) {
+      loadData();
+    }
+  }, [library, account]);
+
   async function loadData() {
-    // Get ethers Web3Provider
-    // Get contract instances
+    let userBalance = await library.getBalance(account);
+    userBalance = parseFloat(ethers.utils.formatEther(`${userBalance}`));
+    console.log(userBalance);
+
+    const signer = library.getSigner(account);
+    const tokenContract = new ethers.Contract(tokenAddress, tokenABI, library);
+    const tokenBalance = await tokenContract.balanceOf(account);
+    console.log(tokenBalance);
     // From crowdSaleContrat: get tokenPrice, tokensSold
     // From tokenContract: totalCrowdSaleSupply, userBalance
+    // window.alert('Contract not deployed to current network. Please change to a supported chain')
   }
 
   return (
