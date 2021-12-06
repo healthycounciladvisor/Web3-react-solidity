b4a0d5fb201e0f8c82cf26373f6258e87e3ed41a
healthycounciladvisor <jimm.dev2323@gmail.com>
2021-12-06 12:17:57 -0700

    Need to update contract addresses after deploying

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,5 +1,5 @@
-export const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const swapExchangeAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const tokenAddress = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+export const swapExchangeAddress = "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82";
 export const tokenABI = [
   {
     inputs: [],
