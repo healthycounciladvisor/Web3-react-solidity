ca037e254a3c277d21ba25bac02618cdc7ff20b1
healthycounciladvisor <jimm.dev2323@gmail.com>
2021-12-06 12:16:47 -0700

    Fixed connector supportedChainIds

--- src/utils/connectors.js
+++ src/utils/connectors.js
@@ -2,12 +2,11 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 
 export const supportedChains = {
   0: "Not connected",
-  //   1: "Mainnet",
   4: "Rinkeby",
   1337: "Dev",
   31337: "Hardhat",
 };
 
 export const injectedConnector = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 31337],
+  supportedChainIds: [1, 3, 4, 1337, 31337],
 });
